873b7ea35fb271fcb6160ef19b174839c309a8be
Jeffrey M. Knerr <knerr@goofydogears.net>
2023-10-21 12:12:54 -0400

    close to matplotlib bingo card, need to fix size

--- makecard.py
+++ makecard.py
@@ -6,51 +6,46 @@ J. Knerr
 Fall 2023
 """
 
-from graphics import *
 import glob
-from random import randrange
 import matplotlib.pyplot as plt
-import numpy as np
+
 
 def main():
-    w = 1000
-    h = 1000
-
-    # Some example data to display
-    x = np.linspace(0, 2 * np.pi, 400)
-    y = np.sin(x ** 2)
-
+    wrkts = readFile("card.txt")
     fig = plt.figure()
     gs = fig.add_gridspec(5, 5, hspace=0, wspace=0)
     axs = gs.subplots(sharex=True, sharey=True)
-#   axs[0, 0].plot(x, y ** 2)
-#   axs[1, 3].plot(x, 0.3 * y, 'o')
-#   axs[2, 1].plot(x, y, '+')
-    plt.tick_params(
-        axis='both',       # changes apply to the x-axis
-        which='both',      # both major and minor ticks are affected
-        bottom=False,      # ticks along the bottom edge are off
-        left=False,      # ticks along the bottom edge are off
-        top=False,         # ticks along the top edge are off
-        labelbottom=False) # labels along the bottom edge are off
-    plt.show()
+    fig.suptitle('B I N G O  Card')
+# https://stackoverflow.com/questions/25124143/get-rid-of-tick-labels-for-all-subplots
+    for ax in axs.flat:
+        ax.set_xticks([])
+        ax.set_yticks([])
+    # add workouts to figure subplots
+    axs = axs.flat
+    for n, ax in enumerate(axs):
+        print(n)
+        print(wrkts[n])
+        ax.text(0.3, 0.45, wrkts[n], size=8)
+#   plt.show()
+    plt.savefig("card.png")
 
 
-def drawLines(gw):
-    """draw the box lines"""
-    W = 3
-    for i in range(6):
-        p1 = Point(i, 0)
-        p2 = Point(i, 5)
-        l = Line(p1, p2)
-        l.setWidth(W)
-        l.draw(gw)
-    for j in range(6):
-        p1 = Point(0, j)
-        p2 = Point(5, j)
-        l = Line(p1, p2)
-        l.setWidth(W)
-        l.draw(gw)
+def readFile(cardfile):
+    """read in the card statefile, return the card workouts"""
+    possible_wkts = readWorkouts()
+    wkts = []
+    inf = open(cardfile, "r")
+    for line in inf:
+        if not line.startswith("#"):
+            i, j, w, d = line.strip().split(",")
+            if w != "FREE":
+                w = int(w)
+                wkts.append("".join(possible_wkts[w-1]))
+            else:
+                wkts.append("FREE")
+    inf.close()
+    return wkts
+
 
 def readWorkouts():
     """read in the workouts"""
@@ -62,5 +57,6 @@ def readWorkouts():
         wkts.append(lines)
         inf.close()
     return wkts
+
 
 main()
